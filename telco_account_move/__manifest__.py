706f7c749f71008d2d0db6105318dc0ea02863b6
Kitcharoen Poolperm <kitcharoenp@gmail.com>
2020-11-19 15:26:28 +0700

    [mod] telco_account_invoice : rename module as `telco_account_move`

--- telco_account_move/__manifest__.py
+++ telco_account_move/__manifest__.py
@@ -4,20 +4,18 @@
 # @license http://opensource.org/licenses/gpl-3.0.html GNU Public License
 
 {
-    'name': 'Telco Account Invoice',
+    'name': 'Telco Vendor Bill',
     'version': '0.01',
     'category': 'Accounting',
     "author": "Kitcharoen Poolperm <kitcharoenp@gmail.com>",
-    'summary': 'Account Invoice',
+    'summary': 'Vendor Bill',
     'description': """
     """,
     'depends': ['account'],
     'data': [
         # Views
-        'views/telco_acc_inv_asset_category_form_view.xml',
-        'views/telco_acc_inv_supplier_purchase_form_view.xml',
-        'views/telco_account_invoice_form_view.xml',
-        'views/telco_account_invoice_tree_view.xml',
+        'views/telco_account_move_form_view.xml',
+        'views/telco_account_move_tree_view.xml',
         # Reports and Templates
     ],
     'installable': True,
