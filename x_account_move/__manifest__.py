416180450ea96239366d3329385e126cfed0cfa6
Kitcharoen Poolperm <kitcharoenp@gmail.com>
2020-12-15 09:17:40 +0000

    [mod] x_account_move : rename `telco_account_move` to `x_account_move`

--- x_account_move/__manifest__.py
+++ x_account_move/__manifest__.py
@@ -4,20 +4,20 @@
 # @license http://opensource.org/licenses/gpl-3.0.html GNU Public License
 
 {
-    'name': 'Telco Vendor Bill',
+    'name': 'X Vendor Bill',
     'version': '0.01',
     'category': 'Accounting',
     "author": "Kitcharoen Poolperm <kitcharoenp@gmail.com>",
-    'summary': 'Vendor Bill',
+    'summary': 'Vendor bill customize',
     'description': """
     """,
     'depends': ['account'],
     'data': [
         # Views
-        'views/telco_account_move_form_view.xml',
-        'views/telco_account_move_tree_view.xml',
+        'views/x_account_move_form_view.xml',
+        'views/x_account_move_tree_view.xml',
         # Reports and Templates
     ],
     'installable': True,
-    'auto_install': False,
+    'application': True,
 }
--- x_account_move/models/account_move.py
+++ x_account_move/models/account_move.py
@@ -9,7 +9,7 @@ from odoo import fields, models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    x_service_order = fields.Char(string='Service Order')
-    x_circuit_id = fields.Char(string='Circuit Id')
-    x_description = fields.Text(string='Description')
+    x_service_ref1 = fields.Char(string='Service Ref.1')
+    x_service_ref2 = fields.Char(string='Service Ref.2')
     x_sap_network = fields.Char(string='Sap Network')
+    x_description = fields.Text(string='Description')
