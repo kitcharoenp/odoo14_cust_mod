47ad36e4d1c6334b3bd6b133e2311c4080a2744e
Kitcharoen Poolperm <kitcharoenp@gmail.com>
2020-12-16 02:04:34 +0000

    [views] x_account_move : rename and change `id` in form and tree view

--- x_account_move/__manifest__.py
+++ x_account_move/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # @author Kitcharoen Poolperm <kitcharoenp@gmail.com>
-# @copyright Copyright (C) 2017
+# @copyright Copyright (C) 2020
 # @license http://opensource.org/licenses/gpl-3.0.html GNU Public License
 
 {
